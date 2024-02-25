b6f7151b9e2214573389d21939a5bfd895ffbdd0
Brian Key <briankey@u.northwestern.edu>
2024-02-24 18:17:21 -0600

    Fit and saved baseline fits; file added to gitignore

--- scripts/3a_fit_baselines.R
+++ scripts/3a_fit_baselines.R
@@ -18,21 +18,45 @@ tidymodels_prefer()
 
 # load recipes ----
 load(here("data/recipes/akt_rec.rda"))
+load(here("data/recipes/basic_rec_5.rda"))
+load(here("data/recipes/basic_rec_10.rda"))
+load(here("data/recipes/basic_rec_20.rda"))
 
 # load folds ----
 load(here("data/splits/train_folds.rda"))
 
 # define model specs ----
-base_spec <- linear_reg() |> 
-  set_mode("regression") |> 
-  set_engine("lm")
+## null ----
+null_spec <- null_model() |> 
+  set_engine("parsnip") |>  
+  set_mode("regression")
 
-# define workflow ----
+## basic ----
+basic_spec <- linear_reg() |> 
+  set_engine("lm") |> 
+  set_mode("regression")
+
+# define workflows ----
+## wfl_set ----
+wfl_set <- workflow_set(
+  preproc = list(
+    neighbors_5 = basic_rec_5,
+    neighbors_10 = basic_rec_10,
+    neighbors_20 = basic_rec_20
+    ),
+  models = list(
+    null = null_spec,
+    lm = basic_spec
+  ),
+  cross = TRUE
+)
+
+## avg_kill_tort ----
 akt_wflw <- workflow() |>  
-  add_model(base_spec) |>  
+  add_model(basic_spec) |>  
   add_recipe(akt_rec)
 
-# fit model ----
+# fit models ----
 ## register cores ----
 registerDoMC(cores = 8)
 
@@ -40,14 +64,30 @@ registerDoMC(cores = 8)
 set.seed(2612)
 
 ## complete fit ----
+### avg_kill_tort ----
 akt_fit <- akt_wflw |> 
   fit_resamples(
     resamples = train_folds, 
     control = control_resamples(save_workflow = TRUE)
   )
 
+wfl_set_fits <- wfl_set |> 
+  workflow_map(
+    "fit_resamples",
+    seed = 1226,
+    resamples = train_folds,
+    control = control_resamples(
+      save_pred = TRUE,
+      save_workflow = TRUE
+    )
+  )
+
+## combine fits ----
+base_fits <- as_workflow_set(akt_wflw = akt_fit) %>% 
+  bind_rows(wfl_set_fits)
+
 ## save fit ----
-akt_fit |> 
+base_fits |> 
   save(
-    file = here("data/results/fits_cv/akt_fit.rda")
+    file = here("data/results/fits_cv/base_fits.rda")
     )
