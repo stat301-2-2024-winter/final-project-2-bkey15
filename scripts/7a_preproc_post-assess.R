e487e9d7b9a2fd78a791b18895d8dc4fc9d7b0a7
Brian Key <briankey@u.northwestern.edu>
2024-02-29 14:29:02 -0600

    Beginning post-assessment work; am trying to impute pop/gdp stats (also from Fariss & missing post-2019) with time series methods to finalize estimates with my best model, as well as use ts methods to predict hr_scores (and compare w/ my model)

--- scripts/7a_preproc_post-assess.R
+++ scripts/7a_preproc_post-assess.R
@@ -1,5 +1,5 @@
 # Final Project ----
-# Initial merge; also create avg_kill_tort
+# Make predictions on missing data
 
 # load packages ----
 library(tidyverse)
@@ -12,6 +12,8 @@ library(naniar)
 library(DT)
 library(plotly)
 library(ggthemes)
+library(fable)
+library(tsibble)
 
 # handle common conflicts
 tidymodels_prefer()
@@ -19,7 +21,7 @@ tidymodels_prefer()
 # load data ----
 ## vdem ----
 vdem <- vdem |> 
-  filter(year > 1945 & year < 2020)
+  filter(year > 1945)
 
 ## hr_scores ----
 hr_scores <- read_csv("data/raw/HumanRightsProtectionScores_v4.01.csv")
@@ -33,7 +35,6 @@ cow_codes <- read_csv("data/raw/COW-country-codes.csv")
 # merge data ----
 ## prep merge
 ### vdem ----
-### BDK: it's essential to recode Czechia's cow_code early on, as it's much more complicated to rectify the missingness that would arise in this instance later on in the process
 vdem <- vdem |> 
   mutate(
     COWcode = if_else(COWcode == 315, 316, COWcode)
@@ -49,14 +50,13 @@ hr_scores <- hr_scores |>
     )
 
 ### pts ----
-### BDK: if you check by including Country in select(), duplicates are Gaza/Palestine, Somaliland, etc; similar to cases w/o hr_scores in initial preproc_data
+### BDK: difference with original is removing filter(year < 2020) argument
 pts <- PTS_2023 |> 
   select(Year, COW_Code_N, PTS_A, PTS_H, PTS_S) |> 
   rename(
     year = Year,
     cowcode = COW_Code_N
     ) |> 
-  filter(year < 2020) |> 
   mutate(
     cow_year = paste(
       cowcode, year, sep = "-"
@@ -67,7 +67,6 @@ pts <- PTS_2023 |>
     )
 
 ### cow_codes ----
-### BDK: we're including and merging this b/c v-dem is missing a lot of country names
 cow_codes <- cow_codes |> 
   select(-StateAbb) |> 
   rename(
@@ -80,7 +79,8 @@ cow_codes <- cow_codes |>
 
 ## complete merge ----
 ### vdem & hr_scores ----
-preproc_data <- right_join(vdem, hr_scores) |> 
+### BDK: note - difference with original is left_join instead of right_join, and removal of duplicates. Note that left_join eliminates the small states, because they appear in hr_scores but not in vdem
+preproc_data <- left_join(vdem, hr_scores) |> 
   rename(
     cowcode = COWcode
   ) |> 
@@ -91,13 +91,10 @@ preproc_data <- right_join(vdem, hr_scores) |>
   ) |> 
   relocate(
     cowcode, year, cow_year, hr_score
-  )
-
-### BDK: check for duplicates - shouldn't have any (i.e., should get an empty set)
-duplicate_test <- preproc_data |> 
+  ) |> 
   filter(
-    duplicated(cow_year)
-    )
+    !is.na(cowcode)
+  )
 
 ### merge preproc & pts ----
 preproc_data <- left_join(preproc_data, pts)
@@ -120,8 +117,7 @@ preproc_data <- preproc_data |>
     )
 
 ## select needed vars only ----
-## BDK: a quick check shows no missingness in country_name_cow; so include this rather than v-dem's country_name
-preproc_data <- preproc_data |> 
+preproc_pa_init <- preproc_data |> 
   select(
     cowcode,
     year,
@@ -180,41 +176,60 @@ preproc_data <- preproc_data |>
     country_name = country_name_cow
     )
 
-# initial missingness check ----
-gg_miss_var(preproc_data)
-
-## BDK: we have missingness in a lot of the v-dem vars. Let's check where avg_kill_tort is missing:
-dattbl_vdem_miss <- preproc_data |> 
-  select(cowcode, country_name, avg_kill_tort) |> 
-  summarize(
-    n_miss = n_miss(avg_kill_tort),
-    .by = c(cowcode, country_name)
-    ) |> 
-  arrange(
-    desc(n_miss)
-    ) |> 
-  datatable(
-    colnames = c(
-      "COW Code", "Country Name", "N Missing"
-    ),
-    style = "bootstrap4"
-    )
-
-## BDK: microstates + post-Soviet/WWII states. The microstates basically have no data whatsoever, so we may need to remove them entirely. We can also remove missing rows for the latter; see notes, but these generally result from coding gaps, coding start dates, etc.
-
-## save data table ----
-dattbl_vdem_miss |> 
-  save(
-    file = here("data/preprocessed/dattbl_vdem_miss.rda")
-    )
-
-dattbl_vdem_miss
-
-# remove missing rows ----
-preproc_data <- preproc_data |> 
+duplicate_check <- preproc_pa_init |> 
   filter(
-    !is.na(avg_kill_tort)
+    duplicated(cow_year)
     )
+
+preproc_pa_init |> 
+  save(
+  file = here("data/preprocessed/post-assess/preproc_pa_init.rda")
+  )
+
+# initial missingness check ----
+gg_miss_var(preproc_data_pa)
+
+# BDK: we have significant missingness in the economic statistics (specific to more recent years), so let's try to impute these with time series forecasting
+
+# time series imputation (e_pop, e_gdp, e_gdppc) ----
+## create tsibble object ----
+ts_preproc <- preproc_pa_init |>
+  select(
+    cowcode,
+    year,
+    country_name,
+    hr_score,
+    e_pop,
+    e_gdp,
+    e_gdppc
+    ) |> 
+  filter(year < 2020) |> 
+  as_tsibble(
+    index = year,
+    key = cowcode
+    )
+
+### save
+ts_preproc |> 
+  save(
+    file = here("data/preprocessed/post-assess/ts_preproc.rda")
+    )
+
+
+registerDoMC(cores = 8)
+
+e_pop_cv <- ts_dat |>
+  stretch_tsibble(.init = 8) |>
+  model(
+    ETS(e_pop),
+    ARIMA(e_pop)
+  )
+
+e_pop_cv_forecasts <- e_pop_cv |> 
+  forecast(h = 1)
+
+e_pop_cv_forecasts_perform_mets <- e_pop_cv_forecasts |> 
+  fabletools::accuracy(ts_dat)
 
 # final missingness check ----
 preproc_miss_plot <- preproc_data |> 
