0644627c2cbdea7e3ca35047deb515cd7e30e5ca
Brian Key <briankey@u.northwestern.edu>
2024-03-07 09:45:47 -0600

    Re-did splits (same observations due to seed ID-ing, different values due to updated dataset)

--- scripts/1a_data_preproc.R
+++ scripts/1a_data_preproc.R
@@ -223,7 +223,8 @@ preproc_miss_plot <- preproc_data |>
     ) |> 
   gg_miss_var() +
   labs(
-    title = "Plot: Missing Observations by Predictor in the Preprocessed Dataset",
+    title = "Plot: Missing Observations by Predictor",
+    subtitle = "Values from the Preprocessed Dataset",
     caption = "Source: V-Dem (2024)"
     ) +
   theme_solarized()
